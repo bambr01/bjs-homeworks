f2787046eefd56ecc13eda9af909c366ab482173
Bely <bambr911@yandex.com>
2020-04-27 17:35:44 +0300

    Выполнена задача 1

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,6 +1,14 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    "use strict"; // код для задачи №1 писать здесь
+    let D = (b**2 - 4*a*c);
+    let x = [];
+    if (D > 0) {
+        x.push((-b + Math.sqrt(D)) / (2*a));
+        x.push((-b - Math.sqrt(D)) / (2*a));
+    } else if (D === 0) {
+        x.push((-b) / (2*a));
+    }
+    return x;
 }
 
 function getAverageMark(marks){
