8c204bbe310a6b5022b1fe0bf10a3d0ba365ba19
Bely <bambr911@yandex.com>
2020-05-02 14:23:43 +0300

    Домашка 1.2 выполнена

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,22 +1,39 @@
 function getResult(a,b,c){
-    "use strict"; // код для задачи №1 писать здесь
-    let D = (b**2 - 4*a*c);
+    "use strict"; 
+    let D = (b ** 2 - 4 * a * c);
     let x = [];
     if (D > 0) {
-        x.push((-b + Math.sqrt(D)) / (2*a));
-        x.push((-b - Math.sqrt(D)) / (2*a));
+        x.push((-b + Math.sqrt(D)) / (2 * a));
+        x.push((-b - Math.sqrt(D)) / (2 * a));
     } else if (D === 0) {
-        x.push((-b) / (2*a));
+        x.push((-b) / (2 * a));
+    } else {
+        x.push();
     }
     return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let sum = 0;
+    let average;
+    if (marks.length === 0) {
+        average = sum;
+    } else if (marks.length >= 5) {
+        marks = marks.slice(0, 5);
+    }
+    for (let i = 0; i < marks.length; i++) {
+        sum = sum + marks[i];
+        average = sum / marks.length;
+    }
+    return average;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let result;
+    if ((new Date().getFullYear() - dateOfBirthday.getFullYear()) < 18) {
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    } else {
+        result = `Не желаете ли олд-фэшн, ${name}?`;
+    }
+    return result;
 }
